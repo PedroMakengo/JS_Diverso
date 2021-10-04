9a99d33d77ace1344d6ab705eea00af0d79cd9f9
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-04 08:29:47 -0300

    Tipos de dados

---
+++ javascrpt/tipos-de-dados/script.js
@@ -0,0 +1,27 @@
+// var idade = 28;
+// var simbol = Symbol();
+
+// console.log(typeof simbol);
+
+// var nome = "Pedro";
+// var sobrenome = "Makengo";
+// var nomeCompleto = nome + " " + sobrenome;
+
+// console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = "Romário fez " + gols + " gols";
+// console.log(typeof frase);
+
+var ano = "2018";
+var mes = 8;
+
+console.log(ano + mes);
+
+var melhor = "teste";
+var frase1 = 'Esse é o "melhor " jogo';
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário faz ${gols} gols`;
+console.log(frase2);
