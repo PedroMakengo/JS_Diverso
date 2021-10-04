12d7153fce457496b37a40462521b5b32c8e6a8e
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-04 13:05:44 -0300

    Melhorando o meu exercício

--- javascrpt/funcoes/script.js
+++ javascrpt/funcoes/script.js
@@ -1,11 +1,7 @@
 // Crie uma função para verificar se um valor é Truthy
 
 function verificarValor(value) {
-  if (!!value) {
-    return `É Truthy`;
-  } else {
-    return "Não é truthy";
-  }
+  return !!value;
 }
 
 console.log(verificarValor(1));
@@ -13,12 +9,11 @@ console.log(verificarValor(1));
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
 
-function perimetro(l1, l2, l3, l4) {
-  var somaPerimetro = l1 + l2 + l3 + l4;
-  return somaPerimetro;
+function perimetro(lado) {
+  return lado * 4;
 }
 
-console.log(perimetro(3, 3, 1, 8));
+console.log(perimetro(3));
 
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
@@ -33,7 +28,7 @@ console.log(nomeCompleto("Pedro", "Miguel"));
 // Crie uma função que verifica se um número é par
 
 function numeroPar(numero) {
-  if (numero % 2 == 0) {
+  if (numero % 2 === 0) {
     return `O número ${numero} é par`;
   } else {
     return `O número ${numero} é impar`;
@@ -50,4 +45,25 @@ function tipoDados(tipo) {
 }
 console.log(tipoDados("Cabelo"));
 
-//
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento scrool ocorrer
+
+addEventListener("scroll", function () {
+  console.log("Pedro Makengo");
+});
+
+// Corrija o erro abaixo
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} paises para visitar`;
+}
+
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+
+console.log(precisoVisitar(20));
+console.log(jaVisitei(20));
