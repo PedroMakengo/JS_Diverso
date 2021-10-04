cb19cfac3f34092f15ab9676c47918fcb575c2f6
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-04 07:59:07 -0300

    Aprendendo sobre variáveis

---
+++ javascrpt/variaveis/script-aula.js
@@ -0,0 +1,28 @@
+var nome = "Pedro";
+var idade = 22;
+var possuirFaculdade = true;
+
+console.log(nome, idade, possuirFaculdade, nome);
+
+var preco = 22;
+var totalComprado = 5;
+
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobrenome = "Makengo",
+  cidade = "Luanda";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// Exercício
+
+var nome = "Pedro Makengo";
+var idade = 22;
+var comidaFavorita;
+comidaFavorita = "Fumbua";
+
+var sobrenome, apelido, curso, escola, classe;
---
+++ javascrpt/variaveis/script.js
@@ -0,0 +1,14 @@
+// Declarar uma variável com o seu nome
+var nome = "Pedro Makengo";
+
+// Declarar uma variável com a sua idade
+var idade = 22;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
