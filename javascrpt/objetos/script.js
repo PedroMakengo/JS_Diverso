256109b37d1d9a2fa083682f6791a44eda03f078
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-06 07:13:29 -0300

    2ª aula de objetos com exercício

--- javascrpt/objetos/script.js
+++ javascrpt/objetos/script.js
@@ -1,18 +1,39 @@
-var pessoa = {
-  nome: "André",
+// Crie um objeto com os seus dados pessoas
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+var dadosPessoas = {
+  nome: "Pedro",
+  sobrenome: "Makengo",
   idade: 22,
-};
+  sexo: "Masculino",
 
-console.log(pessoa);
-
-var quadrado = {
-  lados: 4,
-  area(lado) {
-    return lado * lado;
-  },
-  perimetro(lado) {
-    return this.lados * lado;
+  nomeCompleto() {
+    console.log(`${this.nome} ${this.sobrenome}`);
   },
 };
+// Crie um método no objeto anterior, que mostre o seu nome completo
 
-console.log(quadrado.area(2));
+// Modifique o valor da propriedade preco para 300
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: "Audi",
+};
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador
+// preto com 10 anos, que late ao ver um homem
+
+var cachorro = {
+  raca: "labrador",
+  cor: "preto",
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === "Homem") {
+      return `Latir`;
+    } else {
+      return `Nada`;
+    }
+  },
+};
