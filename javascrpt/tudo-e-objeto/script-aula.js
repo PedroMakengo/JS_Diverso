6f6d9f93b059426ff4c8170e0d658fcbc600a9a2
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-06 08:06:55 -0300

    Terminando os exercícios

---
+++ javascrpt/tudo-e-objeto/script-aula.js
@@ -0,0 +1,5 @@
+var nome = "Pedro";
+
+var nomeMinusculo = nome.toLocaleLowerCase();
+
+var btn = document.querySelector(".btn");
---
+++ javascrpt/tudo-e-objeto/script.js
@@ -0,0 +1,16 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = "Pedro";
+nome.fixed;
+nome.slice;
+nome.length;
+
+// nomeie 5 proprieades ou elmentos do DOM
+var btn = document.querySelector(".btn");
+// btn.addEventListener
+// btn.id
+// btn.innerHTML
+btn.outerHTML;
+console.log(btn.outerHTML);
+
+// busque na web um objeto (método) capaz de interagir com o clipboard
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
