dfa1a198c0d905485881468a278a36f4307fca4b
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-06 11:38:41 -0300

    Efetuando a tarefa

---
+++ javascrpt/atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,19 @@
+var numero = 20;
+var numero2 = 10;
+
+numero += numero2;
+
+console.log(numero);
+console.log(numero2);
+
+var idade = 20;
+var podeBeber;
+naoPossuiDiabetes = true;
+podeBeber = idade >= 18 && naoPossuiDiabetes ? true : false;
+
+console.log(podeBeber);
+
+var possuiFaculdade = false;
+
+if (possuiFaculdade) console.log("Sim possui");
+else console.log("Não possui");
