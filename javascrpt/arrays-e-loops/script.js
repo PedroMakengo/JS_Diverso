c8e6cb344cd72f2ab7300dff955a852e8557f308
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-06 10:48:51 -0300

    Aprendendo sobre loops

---
+++ javascrpt/arrays-e-loops/script.js
@@ -0,0 +1,13 @@
+var videoGames = ["Switch", "PS4", "XBox"];
+
+var ultimoItem = videoGames.pop();
+
+for (var numero = 1; numero <= 10; numero++) {
+  console.log(numero);
+}
+
+var i = 0;
+while (i <= 100) {
+  console.log(i);
+  i = i + 5;
+}
