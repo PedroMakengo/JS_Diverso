dc6b461a1314c7ed0b59f6a13e36eccce0f4d882
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-07 06:39:10 -0300

    Entendendo a sintaxe do javascript

--- javascrpt/escopo/script.js
+++ javascrpt/escopo/script.js
@@ -4,9 +4,9 @@
   var cor = "Preto";
   const marca = "Fiat";
   let portas = 4;
-}
 
-// console.log(cor, marca, portas);
+  console.log(cor, marca, portas);
+}
 
 // Como  corrigir o erro abaixo
 const dois = 2;
@@ -23,9 +23,9 @@ somarDois(4);
 dividirDois(6);
 
 // O que fazer para total retornar 500
-var numero = 50;
-for (var numero = 0; numero < 10; numero++) {
+const numero = 50;
+for (let numero = 0; numero < 10; numero++) {
   console.log(numero);
 }
 
-console.log(numero * 50);
+console.log(numero * 10);
