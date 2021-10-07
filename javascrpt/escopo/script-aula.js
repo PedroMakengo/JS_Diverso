9a61c4834e5ac6a4ac2dbe5994e9f92e770e06fa
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-07 06:33:18 -0300

    Escopo de bloco

---
+++ javascrpt/escopo/script-aula.js
@@ -0,0 +1,33 @@
+var carro = "Fusca";
+
+function mostrarCarro() {
+  console.log(carro);
+}
+
+mostrarCarro();
+console.log(carro);
+
+if (true) {
+  var mes = "Outubro";
+  console.log(mes);
+}
+
+console.log(mes);
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(`Número ${i}`);
+// }
+
+// console.log(i * 10);
+
+const semana = "Sexta";
+// semana = "Quinta";
+
+console.log(semana);
+
+const data = {
+  ano: 2021,
+  mes: "Dezembro",
+};
+
+data.dia = 7;
