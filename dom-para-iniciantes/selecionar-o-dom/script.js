724f39e0a9ebe21e4be2a44fd9fadeac68eb66b9
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-09 05:06:55 -0300

    Selecção de elementos DOM

---
+++ dom-para-iniciantes/selecionar-o-dom/script.js
@@ -0,0 +1,19 @@
+const animais = document.getElementById("animais");
+console.log(animais);
+
+const gridSection = document.getElementsByClassName("grid-section");
+
+console.log(gridSection[1]);
+
+const primeiraLi = document.querySelector("li");
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector("ul");
+console.log(primeiraUl);
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos.href);
+
+const animaisImg = document.querySelectorAll(".animais img");
+
+console.log(animaisImg);
