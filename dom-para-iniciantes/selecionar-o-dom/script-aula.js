e57d325647d8a4d349e1f7437a10467e349a35f7
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-09 15:20:27 -0300

    Adicionando

---
+++ dom-para-iniciantes/selecionar-o-dom/script-aula.js
@@ -0,0 +1,22 @@
+const animais = document.getElementById("animais");
+console.log(animais);
+
+const gridSection = document.getElementsByClassName("grid-section");
+
+console.log(gridSection[1]);
+
+const primeiraLi = document.querySelector("li");
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector("ul");
+console.log(primeiraUl);
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos.href);
+
+const animaisImg = document.querySelectorAll(".animais img");
+
+console.log(animaisImg);
+
+const gridSectionHTML = document.getElementsByClassName("grid-section");
+const gridSectionNode = document.querySelectorAll(".grid-section");
