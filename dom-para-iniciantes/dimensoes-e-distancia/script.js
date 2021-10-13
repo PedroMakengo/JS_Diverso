b7ca56acf007ac5b19acb31c98a01a6c5c93f986
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-13 05:59:42 -0300

    Aprendendo sobre dimensões e distância

--- dom-para-iniciantes/dimensoes-e-distancia/script.js
+++ dom-para-iniciantes/dimensoes-e-distancia/script.js
@@ -7,3 +7,36 @@ imagens.forEach((item, index) => {
     console.log(item.offsetTop);
   }
 });
+
+// Retorne a soma da largura de todas as imagens
+window.onload = () => {
+  let soma = 0;
+  imagens.forEach((imagem) => {
+    soma += imagem.offsetWidth;
+    console.log(soma);
+  });
+};
+
+// Verifique se os links da página possuem
+// O mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll("a");
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+
+  if (linkWidth > 48 && linkHeight > 48) {
+    console.log(link, "Possui boa acessibilidade");
+  } else {
+    console.log(link, "Não possui boa acessibilidade");
+  }
+});
+
+// Se o browser for menor que 720px
+// adicione a classe menu-mobile ao menu
+const browserSmall = window.matchMedia("(max-width: 720px)").matches;
+
+if (browserSmall) {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("menu-mobile");
+}
