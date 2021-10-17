1dbe4d2eca1c38c6ccfa643d5f8bb9771034fdc2
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-17 17:27:31 -0300

    Efetuando a primeira tarefa

--- dom-para-iniciantes/eventos/script.js
+++ dom-para-iniciantes/eventos/script.js
@@ -2,32 +2,38 @@
 // adicione a classe ativo ao item clicado e remova dos
 // demais itens caso eles possuam a mesma. Previna
 // o comportamento padrão desses link
-const link = document.querySelector('a[href^="#"]');
-link.addEventListener("click", function (event) {
-  event.preventDefault();
+const linkInternos = document.querySelectorAll("a[href^='#']");
 
-  console.log((event.target.classList.add = "ative"));
-  console.log(event.target);
+function handlLink(event) {
+  event.preventDefault();
+  linkInternos.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  this.classList.add("ativo");
+}
+
+linkInternos.forEach((link) => {
+  link.addEventListener("click", handlLink);
 });
 
 // Selecione todos os elemntos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
 
-const el = document.querySelector("*");
-function handleClicado(event) {
-  console.log(event.target);
-}
-el.addEventListener("click", handleClicado);
+// const el = document.querySelector("*");
+// function handleClicado(event) {
+//   console.log(event.target);
+// }
+// el.addEventListener("click", handleClicado);
 
 // Utilizando o código anterior, ao invés de mostrar no console
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
 
 // Se o usuário clicar na tecla t
 
-function handleClicado(event) {
-  event.target.remove();
-  if (event.key === "t") {
-    document.body.style.fontSize = "30px";
-  }
-}
-el.addEventListener("click", handleClicado);
+// function handleClicado(event) {
+//   event.target.remove();
+//   if (event.key === "t") {
+//     document.body.style.fontSize = "30px";
+//   }
+// }
+// el.addEventListener("click", handleClicado);
