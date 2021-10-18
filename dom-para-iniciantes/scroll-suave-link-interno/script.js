cd4e7b24355d752e7b8a27d49e2bbd2210b751c9
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-18 12:48:39 -0300

    Adicionando comentários

--- dom-para-iniciantes/scroll-suave-link-interno/script.js
+++ dom-para-iniciantes/scroll-suave-link-interno/script.js
@@ -4,13 +4,18 @@ function initTabNav() {
 
   if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add("ativo");
+    // Função para ativar o tab
     function activeTab(index) {
+      // Percorrendo todas as section e removendo a class ativo
       tabContent.forEach((section) => {
         section.classList.remove("ativo");
       });
+      // Adicionando a class ativo na section de acordo ao index da imagem
       tabContent[index].classList.add("ativo");
     }
 
+    // Percorrendo todas imagens como tab e usar o index destas imagens
+    // como parametro da minha função
     tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener("click", () => {
         activeTab(index);
