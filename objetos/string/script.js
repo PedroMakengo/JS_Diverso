1e339df2716928e77680342a9ac395781d0bd7d1
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-26 11:48:04 -0300

    Efetuando exercício sobre string

--- objetos/string/script.js
+++ objetos/string/script.js
@@ -13,30 +13,75 @@
 
 // const fraseFinal = frase.concat(linguagem, "!!");
 
-const fruta = "Banana";
-const listaFrutas = "Melancia, Banana, Laranja";
+// const fruta = "Banana";
+// const listaFrutas = "Melancia, Banana, Laranja";
 
-console.log(listaFrutas.includes(fruta, 1));
+// console.log(listaFrutas.includes(fruta, 1));
 
-console.log(fruta.startsWith("Ba")); // Começa com...
-console.log(fruta.endsWith("na")); // Termina com...
+// console.log(fruta.startsWith("Ba")); // Começa com...
+// console.log(fruta.endsWith("na")); // Termina com...
 
 // Transação
-const transacao1 = "Depósito de cliente";
-const transacao2 = "Depósito de fornecedor";
-const transacao3 = "Taxa de camisas";
+// const transacao1 = "Depósito de cliente";
+// const transacao2 = "Depósito de fornecedor";
+// const transacao3 = "Taxa de camisas";
 
-console.log(transacao1.slice(0, 3));
+// console.log(transacao1.slice(0, 3));
 
-const lingugagem = "Javascript";
+// const lingugagem = "Javascript";
 
-lingugagem.substring(-5); // substring não retorna valores negativos
+// lingugagem.substring(-5); // substring não retorna valores negativos
 
-console.log(fruta.indexOf("B")); // Retorna o index de uma string
+// console.log(fruta.indexOf("B")); // Retorna o index de uma string
 
-const listaPrecos = ["R$ 99", "R$ 199", "R$ 12000"];
-listaPrecos.forEach((preco) => {
-  // console.log(preco.padStart(10, "0")); // Acrescenta caracteres dentro da
-  // minha string de acordo aos meus parametros
-  console.log(preco.padEnd(10, "0"));
-});
+// const listaPrecos = ["R$ 99", "R$ 199", "R$ 12000"];
+// listaPrecos.forEach((preco) => {
+//   // console.log(preco.padStart(10, "0")); // Acrescenta caracteres dentro da
+//   // minha string de acordo aos meus parametros
+//   console.log(preco.padEnd(10, "0"));
+// });
+
+// const fruta2 = "Ta";
+// console.log(fruta2.repeat(5));
+
+// fruta.repeat(20);
+
+let listaItens = "Camisas Bonés Calças Bermudas Vestidos Saias";
+
+listaItens = listaItens.replace(/[ ]+/g, ", ");
+console.log(listaItens);
+
+let preco = "R$ 1200,43";
+
+preco = preco.replace(",", ".");
+console.log(preco);
+
+const arrayList = listaItens.split(", "); // retorna um array com as posições
+// de acordo ao parametro especificado
+console.log(arrayList);
+
+const htmlText = "<div>O melhor item</div><div>A melhor lista</div>";
+const htmlArray = htmlText.split("div");
+const novoHtml = htmlArray.join("section");
+
+console.log(htmlText);
+console.log(htmlArray);
+console.log(novoHtml);
+
+const frutaArray = ["Banana", "Melancia", "Laranja"];
+
+// toLowerCase() letra maiuscula
+// toUpperCase() letra minuscula
+
+const sexo1 = "Feminino";
+const sexo2 = "feminino";
+const sexo3 = "FEMININO";
+
+console.log(sexo1.toLowerCase() === "FEMININO");
+
+// Remover espaço
+// trim(), trimStart, trimEnd
+const valor = "  R$ 23   ";
+valor.trim();
+
+console.log(valor);
