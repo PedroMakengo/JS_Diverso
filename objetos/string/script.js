e32145041395f2abe89efa7b0a94eeb33e05cdff
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-26 10:35:52 -0300

    Trabalhando com string e aprendendo sobre os metodos correspondente a string

---
+++ objetos/string/script.js
@@ -0,0 +1,29 @@
+// const comida = "Pizza";
+// const agua = new String("Agua");
+
+// console.log(agua.length);
+
+// const frase = "A melhor comida";
+
+// console.log(frase[frase.length]);
+// console.log(frase.charAt(0));
+
+// const frase = "A minha linguagem é ";
+// const linguagem = "JavaScript";
+
+// const fraseFinal = frase.concat(linguagem, "!!");
+
+const fruta = "Banana";
+const listaFrutas = "Melancia, Banana, Laranja";
+
+console.log(listaFrutas.includes(fruta, 1));
+
+console.log(fruta.startsWith("Ba")); // Começa com...
+console.log(fruta.endsWith("na")); // Termina com...
+
+// Transação
+const transacao1 = "Depósito de cliente";
+const transacao2 = "Depósito de fornecedor";
+const transacao3 = "Taxa de camisas";
+
+console.log(transacao1.slice(0, 3));
