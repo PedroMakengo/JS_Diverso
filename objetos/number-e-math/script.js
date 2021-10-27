db2bca3cf8498ffe1bc587e5661a07e24c52d060
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-27 16:50:45 -0300

    Aprendendo sobre Number e Math

--- objetos/number-e-math/script.js
+++ objetos/number-e-math/script.js
@@ -1,7 +1,16 @@
 const ano = 2018;
-const preco = new Number(2019);
+const preco = 2.99;
 
 // Métodos
 console.log(Number.isNaN(5)); // Verificar se o valor é true ou false
-
 console.log(Number.isInteger(20)); // Verificar se o valor é um número inteiro
+
+// parseFloat
+console.log(Number.parseFloat("99.50"));
+console.log(Number.parseInt("100.50"));
+
+console.log(preco.toFixed());
+
+const carro = 1000.455;
+
+console.log(carro.toFixed(2));
