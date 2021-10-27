816f2b599d1cbd448abd7182209b0db9b6a1d8bc
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-27 16:45:13 -0300

    Aprendendo sobre métodos de string

---
+++ objetos/number-e-math/script.js
@@ -0,0 +1,7 @@
+const ano = 2018;
+const preco = new Number(2019);
+
+// Métodos
+console.log(Number.isNaN(5)); // Verificar se o valor é true ou false
+
+console.log(Number.isInteger(20)); // Verificar se o valor é um número inteiro
