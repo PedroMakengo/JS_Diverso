8c513abfdf2d31c2d82b895bc28e5e2ef0ce5a23
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-11-01 16:25:04 -0300

    Aprendendo sobre os métodos de objeto

--- objetos/object/script.js
+++ objetos/object/script.js
@@ -1,23 +1,91 @@
-const pessoa = new Object({
-  nome: "Pedro Makengo",
-});
+// const pessoa = new Object({
+//   nome: "Pedro Makengo",
+// });
 
-console.log(pessoa);
+// console.log(pessoa);
 
-const carro = {
-  rodas: 4,
-  init(valor) {
-    this.marca = valor;
-    return this;
-  },
-  acelerar() {
-    return this.marca + " acelerou";
-  },
-  buzinar() {
-    return this.marca + " buzinou";
-  },
+// const carro = {
+//   rodas: 4,
+//   init(valor) {
+//     this.marca = valor;
+//     return this;
+//   },
+//   acelerar() {
+//     return this.marca + " acelerou";
+//   },
+//   buzinar() {
+//     return this.marca + " buzinou";
+//   },
+// };
+
+// const honda = Object.create(carro).init("Honda");
+// console.log(honda.acelerar());
+
+// const ferrari = Object.create(carro).init("Ferrari");
+// console.log(ferrari.acelerar());
+
+// const funcaoAutomovel = {
+//   acelerar() {
+//     return "acelerou";
+//   },
+//   buzinar() {
+//     return "buzinou";
+//   },
+// };
+
+// const motas = {
+//   rodas: 2,
+//   capacete: true,
+// };
+
+// Object.assign(motas, funcaoAutomovel, carro);
+
+// console.log(motas);
+
+const moto = {
+  capacete: true,
 };
 
-const honda = Object.create(carro);
-honda.init("Honda");
-console.log(honda);
+Object.defineProperties(moto, {
+  rodas: {
+    get() {
+      return this._rodas;
+    },
+    set(valor) {
+      this._rodas = valor * 4;
+    },
+  },
+});
+
+// moto.rodas = 3;
+// console.log(moto);
+
+// const propriedades = Object.getOwnPropertyDescriptors( moto);
+// console.log(propriedades);
+
+const innerHeightConfig = Object.getOwnPropertyDescriptor(
+  window,
+  "innerHeight"
+);
+
+Object.getOwnPropertyNames(Array.prototype);
+
+const frutas = ["Banana"];
+
+console.log(Object.getPrototypeOf(""));
+console.log(Array.prototype);
+
+const frutas1 = ["Banana", "Pêra"];
+const frutas2 = ["Banana", "Pêra"];
+
+Object.is(frutas1, frutas2);
+
+const carro = {
+  marca: "Ford",
+  ano: 2018,
+};
+
+// Object.freeze(carro);
+
+carro.marca = "Honda";
+console.log(carro);
