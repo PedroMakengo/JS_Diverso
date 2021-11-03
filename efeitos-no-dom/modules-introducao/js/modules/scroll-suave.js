93b8390db6a0c62edc6c28faa4d85ac70beea286
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-11-03 09:21:04 -0300

    Terminando a minha modal

--- efeitos-no-dom/modules-introducao/js/modules/scroll-suave.js
+++ efeitos-no-dom/modules-introducao/js/modules/scroll-suave.js
@@ -15,6 +15,13 @@ export default function initScrollSuave() {
         behavior: "smooth",
         block: "start",
       });
+
+      // forma alternativa
+      // const topo = section.offsetTop;
+      // window.scrollTo({
+      //   top: topo,
+      //   behavior: "smooth"
+      // })
     }
 
     linksInternos.forEach((link) => {
