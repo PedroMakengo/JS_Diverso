852df5f63212406e44b6ec998299c9dded79f0fc
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-11-10 23:57:31 -0300

    Estudando settimeout

---
+++ efeitos-no-dom/settimout-e-sentinterval/js/modules/scroll-animacao.js
@@ -0,0 +1,18 @@
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+
+      if (isSectionVisible) section.classList.add("ativo");
+      else section.classList.remove("ativo");
+    });
+  }
+
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
---
+++ efeitos-no-dom/settimout-e-sentinterval/js/script.js
@@ -0,0 +1,21 @@
+// import initScrollSuave from "./modules/scroll-suave.js";
+// import initAnimacaoScroll from "./modules/scroll-animacao.js";
+// import initAccordion from "./modules/accordion.js";
+// import initTabNav from "./modules/tabnav.js";
+// import initModal from "./modules/modal.js";
+// import initTooltip from "./modules/tooltip.js";
+// import initDropdownMenu from "./modules/dropdown-menu.js";
+
+// initScrollSuave();
+// initAnimacaoScroll();
+// initAccordion();
+// initTabNav();
+// initModal();
+// initTooltip();
+// initDropdownMenu();
+
+function espera(texto) {
+  console.log(texto);
+}
+
+setTimeout(espera, 2000, "Passou 1s");
