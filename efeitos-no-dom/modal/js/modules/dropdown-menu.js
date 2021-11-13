6509c2bf41b5503e771a579d69578d43f83db6c5
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-11-13 02:24:05 -0300

    Efeitos-no-dom

--- efeitos-no-dom/modal/js/modules/dropdown-menu.js
+++ efeitos-no-dom/modal/js/modules/dropdown-menu.js
@@ -11,4 +11,8 @@ dropdownMenus.forEach((menu) => {
 function handleClick(event) {
   event.preventDefault();
   this.classList.add("active");
+
+  outsideClick(this, ["touchstart", "click"], () => {
+    this.classList.remove("active");
+  });
 }
