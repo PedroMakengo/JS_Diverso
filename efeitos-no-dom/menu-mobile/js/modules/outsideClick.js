8ed1de5803d41eb6102d499c99d431abbcd02116
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-11-15 12:03:15 -0300

    Terminando o meu menu

--- efeitos-no-dom/menu-mobile/js/modules/outsideClick.js
+++ efeitos-no-dom/menu-mobile/js/modules/outsideClick.js
@@ -4,14 +4,13 @@ export default function outsideClick(element, events, callback) {
 
   if (!element.hasAttribute(outside)) {
     events.forEach((userEvent) => {
-      html.addEventListener(userEvent, handleOutsideClick);
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
     });
     element.setAttribute(outside, "");
   }
-
   function handleOutsideClick(event) {
     if (!element.contains(event.target)) {
-      element.removeAttribute(event.target);
+      element.removeAttribute(outside);
       events.forEach((userEvent) => {
         html.removeEventListener(userEvent, handleOutsideClick);
       });
