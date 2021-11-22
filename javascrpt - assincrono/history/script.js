25a42b0f34256bcb3926929da6ef284a3ccf2c06
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-11-22 15:32:21 -0300

    Pegando animais por meio de uma api

--- javascrpt - assincrono/history/script.js
+++ javascrpt - assincrono/history/script.js
@@ -1,13 +1,33 @@
 const links = document.querySelectorAll("a");
 
 function handleClick(event) {
-  event.preventDefault();
+  event.preventDefault(".content");
   fetchPage(event.target.href);
+  window.history.pushState(null, null, event.target.href);
 }
 
-function fetchPage(url) {
-  console.log(url);
+async function fetchPage(url) {
+  document.querySelector(".content").innerHTML = "Carregando";
+  const pageResponse = await fetch(url);
+  const pageText = await pageResponse.text();
+
+  replaceContent(pageText);
 }
+
+function replaceContent(newText) {
+  const newHtml = document.createElement("div");
+  newHtml.innerHTML = newText;
+
+  const oldCOntent = document.querySelector(".content");
+  const newContent = newHtml.querySelector(".content");
+
+  oldCOntent.innerHTML = newContent.innerHTML;
+  document.title = newHtml.querySelector("title").innerText;
+}
+
+window.addEventListener("popstate", () => {
+  fetchPage(window.location.href);
+});
 
 links.forEach((link) => {
   link.addEventListener("click", handleClick);
