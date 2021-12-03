d8c994b7edee2255f17529ec50114e8c6ddfb6c2
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-03 18:26:09 -0300

    Efetuando a tarefa

--- 07-mais-javascript/loops-e-iterable/script.js
+++ 07-mais-javascript/loops-e-iterable/script.js
@@ -1,36 +1,9 @@
-// const frutas = ["Banana", "Morango", "Uva"];
-// const frase = "Isso é JavaScript";
+const items = document.querySelectorAll("li");
 
-// // fetch("https://pokeapi.com/api/v2/pokemon/").then(({ headers }) => {
-// //   console.log(headers);
-// // });
-
-// for (const fruta of frutas) {
-//   console.log(fruta);
-// }
-
-// for (const char of frase) {
-//   console.log(char);
-// }
-
-// const buttons = document.querySelectorAll("button");
-
-// for (const btn of buttons) {
-//   btn.style.color = "blue";
-// }
-
-const carro = {
-  marca: "Honda",
-  ano: 2021,
-};
-
-for (const key in carro) {
-  console.log(key, carro[key]);
+for (const item of items) {
+  item.classList.add("ativo");
 }
 
-const btnUnico = document.querySelector("button");
-const btnStyles = getComputedStyle(btnUnico);
-
-for (const style in btnStyles) {
-  console.log(`${style}: ${btnStyles[style]}`);
+for (const windowKey in window) {
+  console.log(windowKey + " " + window[windowKey]);
 }
