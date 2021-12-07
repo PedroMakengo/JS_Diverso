424f72063e81e72183ad3a41c74dd2931046f7c6
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-07 16:58:16 -0300

    Apreendendo sobre regeax

--- 08-regeax/loops-e-iterable/script.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll("li");
-
-for (const item of items) {
-  item.classList.add("ativo");
-}
-
-for (const windowKey in window) {
-  console.log(windowKey + " " + window[windowKey]);
-}
