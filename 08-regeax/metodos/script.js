8f5199f23cb9b762bfe39d943494fa504533fe67
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-13 08:11:29 -0300

    Trabalhando com regexp

--- 08-regeax/metodos/script.js
+++ 08-regeax/metodos/script.js
@@ -9,7 +9,7 @@
 // const resultado = text.replace(regexp, "");
 
 // CPFs
-// const regexp = /(?:\d{3}[.-]?){3}\d{2}/;
+// const regexp = /(?:\d{3}[.-]?){3}\d{2}/g;
 
 // const text = `
 //       000.000.000-00
@@ -22,14 +22,53 @@
 
 // console.log(resultado);
 
-const regexp = /\d{2}[-.]?(?:\d{3}[-.]?){2}\/?\d{4}[-.]?\d{2}/;
+// const regexp = /\d{2}[-.]?(?:\d{3}[-.]?){2}\/?\d{4}[-.]?\d{2}/g;
 
-const text = `
-     00.000.000/0000-00
-     00000000000000
-     00-000-000-0000-00 
-     00.000.000/000000
-     00.000.000.000000
-     00.000.000.0000.00
+// const text = `
+//      00.000.000/0000-00
+//      00000000000000
+//      00-000-000-0000-00
+//      00.000.000/000000
+//      00.000.000.000000
+//      00.000.000.0000.00
+// `;
 
+// const regexpTelefone = /(?:\+?55\s?)?(?:\(?\d{2}\)?[\s-]?)?\d{4,5}[-\s]?\d{4}/g;
+
+// const telefones = `
+//     +55 11 98888-8888
+//     +55 11 98888 8888
+//     +55 11 988888888
+//     +55 11988888888
+//     +5511988888888
+//     5511988888888
+//     11 98888-8888
+//     11 98888 8888
+//     (11) 98888 8888
+//     (11) 98888-8888
+//     11-98888-8888
+//     11 98888 8888
+//     11988888888
+//     11988888888
+//     (11)988888888
+//     98888 8888
+//     8888 8888
+// `;
+
+// const resultado = telefones.replace(regexpTelefone, "");
+
+const regexpEmail = /[\w.-]+@[\w-]+\.[\w.-]+/gi;
+
+const emails = `
+      email@email.com
+      email@email.com.org
+      email-email@email.com 
+      email_email@email.com
+      email.email123@email.com.br
+      email.email23@empresa-sua.com.br
+      c@contato.cc
 `;
+
+const resultado = emails.replace(regexpEmail, "");
+
+console.log(resultado);
