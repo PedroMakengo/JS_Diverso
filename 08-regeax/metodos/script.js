c41063ae5d9e75274dc1b5bfba3dc5f5b2f696b4
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-13 11:03:42 -0300

    Terminando o exercício

--- 08-regeax/metodos/script.js
+++ 08-regeax/metodos/script.js
@@ -15,11 +15,63 @@
 //   console.log(i++, regexp.lastIndex);
 // }
 
-const frase = "JavaScript, TypeScript, CoffeeScript, Java";
+// const frase = "JavaScript, TypeScript, CoffeeScript, Java";
 
-const regexp = /\w+/g;
+// const regexp = /\w+/g;
 
-let regexpResult;
-while ((regexpResult = regexp.exec(frase)) !== null) {
-  console.log(regexpResult[0]);
-}
+// let regexpResult;
+// while ((regexpResult = regexp.exec(frase)) !== null) {
+//   console.log(regexpResult[0]);
+// }
+
+// const frase = "JavaScript, TypeScript, CoffeeScript, Java, 200";
+
+// const regexp = /[A-Za-z]+/g;
+
+// const resultados = frase.split(regexp);
+
+// console.log(resultados);
+
+// const tags = `
+//   <ul>
+//     <li>Item 1</li>
+//     <li>Item 2</li>
+//   </ul>
+// `;
+
+// const regexp = /(?<=<\/?)\w+/g;
+
+// const resultado = tags.replace(regexp, "div");
+// console.log(resultado);
+
+// const emails = `
+//   empresa@email.com
+//   contato@email.com
+//   suporte@email.com
+// `;
+
+// const regexp = /(\w+@)[\w.]+/g;
+
+// const resultado = emails.replace(regexp, "$1gmail.com");
+
+// console.log(resultado);
+
+const emails = `
+  joao@homail.com.br
+  marta@ggmail.com.br
+  bruno@oulook.com.br
+`;
+
+const regexp = /(\w+@)[\w.]+/g;
+
+const resultado = emails.replace(regexp, function (...args) {
+  if (args[2] === "homail.com.br") {
+    return args[1] + "hotmail.com.br";
+  } else if (args[2] === "ggmail.com.br") {
+    return args[1] + "gmail.com.br";
+  } else if (args[2] === "oulook.com.br") {
+    return args[1] + "outlook.com.br";
+  } else {
+    return args[0];
+  }
+});
