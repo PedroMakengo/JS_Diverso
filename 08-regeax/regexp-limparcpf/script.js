611396bb18632c26c3a604ae9105220dd7d114d7
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-13 11:30:28 -0300

    Limpando o meu CPF

--- 08-regeax/regexp-limparcpf/script.js
+++ 08-regeax/regexp-limparcpf/script.js
@@ -1,13 +1,28 @@
-function contagem() {
-  let total = 0;
-  return function incremento() {
-    total++;
-    console.log(total);
-  };
-}
+const cpfsList = document.querySelectorAll(".cpf li");
 
-const ativarIncremento = contagem();
+const elementsInnerText = ([...elements]) => {
+  return elements.map((element) => element.innerText);
+};
 
-setInterval(() => {
-  ativarIncremento();
-}, 1000);
+const limparCPF = (cpf) => {
+  return cpf.replace(/\D/g, "");
+};
+
+const construirCPF = (cpf) => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
+};
+
+const formatarCPFS = (cpfs) => {
+  return cpfs.map(limparCPF).map(construirCPF);
+};
+
+const substituiCPFS = (cpfsElements) => {
+  const cpfs = elementsInnerText(cpfsElements);
+  const cpfsFormatados = formatarCPFS(cpfs);
+
+  cpfsElements.forEach((element, index) => {
+    element.innerText = cpfsFormatados[index];
+  });
+};
+
+substituiCPFS(cpfsList);
