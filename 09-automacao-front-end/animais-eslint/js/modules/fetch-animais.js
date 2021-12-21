c7433b2f511d76764c4dce518864c103c044f028
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-21 05:45:06 -0300

    Trabalhando no fetch-animais

--- 09-automacao-front-end/animais-eslint/js/modules/fetch-animais.js
+++ 09-automacao-front-end/animais-eslint/js/modules/fetch-animais.js
@@ -1,11 +1,20 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import initAnimaNumeros from './anima-numeros.js';
 
 export default function initFetchAnimais() {
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+
+    div.innerHTML = `<h3>${animal.space}</h3> <span data-numero>${animal.total}</span>`;
+
+    return div;
+  }
+
   async function fetchAnimais(url) {
     try {
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
-      const numeroGrid = document.querySelector(".numeros-grid");
+      const numeroGrid = document.querySelector('.numeros-grid');
 
       animaisJSON.forEach((animal) => {
         const divAnimal = createAnimal(animal);
@@ -18,14 +27,5 @@ export default function initFetchAnimais() {
     }
   }
 
-  function createAnimal(animal) {
-    const div = document.createElement("div");
-    div.classList.add("numero-animal");
-
-    div.innerHTML = `<h3>${animal.space}</h3> <span data-numero>${animal.total}</span>`;
-
-    return div;
-  }
-
-  fetchAnimais("./animaisapi.json");
+  fetchAnimais('./animaisapi.json');
 }
