ef33d388de16387dc2a9e95e9d6690bcbe7814cd
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-23 07:09:40 -0300

    Refatorando o meu projeto

--- 10-projeto-final/js/modules/fetch-bitcoin.js
+++ 10-projeto-final/js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
 export default function initFetchBitcoin() {
-  fetch("https://blockchain.info/ticker")
-    .then((response) => response.json())
+  fetch('https://blockchain.info/ticker')
+    .then(response => response.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector(".btn-preco");
+      const btcPreco = document.querySelector('.btn-preco');
       btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
     })
     .catch((error) => {
